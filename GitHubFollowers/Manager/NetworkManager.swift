b0baa4df288bef50f24d9fe258bfaaea0c0e3371
Joe McCraw <joe@showblender.com>
2020-01-05 11:09:09 -0800

    Create NetworkManager Singleton
    private init with shared static property

---
+++ GitHubFollowers/Manager/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  GitHubFollowers
+//
+//  Created by Joseph McCraw on 1/5/20.
+//  Copyright © 2020 Joseph McCraw. All rights reserved.
+//
+
+import Foundation
+
+// Network Singleton
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {
+        
+    }
+}

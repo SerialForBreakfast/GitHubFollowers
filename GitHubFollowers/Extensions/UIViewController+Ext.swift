3b3a942bece0df3d48889c952724a0bff9260bbf
Joe McCraw <joe@showblender.com>
2020-01-13 16:59:38 -0800

    Added LoadingView
    Small Organizational update too

--- GitHubFollowers/Extensions/UIViewController+Ext.swift
+++ GitHubFollowers/Extensions/UIViewController+Ext.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+fileprivate var containerView: UIView!
 
 extension UIViewController {
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
@@ -15,6 +16,29 @@ extension UIViewController {
                         let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
             alertVC.modalPresentationStyle = .overFullScreen
             self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+    
+    func showloadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        activityIndicator.startAnimating()
+    }
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -54,9 +54,11 @@ class FollowerListVC: UIViewController {
     
     
     func getFollowers(username: String, page: Int) {
+        showloadingView()
+//        #warning("Call Dismiss")
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
-            
+            self.dismissLoadingView()
             switch result {
                 case .success(let followers):
                     if followers.count < 100 {

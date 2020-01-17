f5d2278ce09821ee0309b0111a227469e98d5d7b
Joe McCraw <joe@showblender.com>
2020-01-17 15:30:23 -0800

    Added search feature

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -15,6 +15,7 @@ class FollowerListVC: UIViewController {
     
     var username: String!
     var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
     var page: Int = 1
     var hasMoreFollowers = true
     
@@ -26,6 +27,7 @@ class FollowerListVC: UIViewController {
         //        print(username)
         configureViewController()
         configureCollectionView()
+        configureSearchController()
         getFollowers(username: username, page: page)
         configureDataSource()
     }
@@ -51,7 +53,15 @@ class FollowerListVC: UIViewController {
         
     }
     
-    
+    func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Search for a Username"
+        searchController.obscuresBackgroundDuringPresentation   = false
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.searchController = searchController
+    }
     
     func getFollowers(username: String, page: Int) {
         showloadingView()
@@ -68,13 +78,13 @@ class FollowerListVC: UIViewController {
                 
                 if followers.isEmpty {
                     let message = "This user has no followers. 🙁"
-                    print(message)
+//                    print(message)
                     DispatchQueue.main.async {
                         self.showEmptyStateView(with: message, in: self.view)
                     }
                     return
                 }
-                self.updateData()
+                self.updateData(on: followers)
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something bad happened", message: error.rawValue, buttonTitle: "ok")
             }
@@ -89,7 +99,7 @@ class FollowerListVC: UIViewController {
             return cell
         })
     }
-    func updateData() {
+    func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -113,3 +123,19 @@ extension FollowerListVC: UICollectionViewDelegate {
         }
     }
 }
+
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else {
+            updateData(on: followers)
+            return }
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased())}
+        updateData(on: filteredFollowers)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print("cancel Tapped")
+        updateData(on: followers)
+    }
+}
+

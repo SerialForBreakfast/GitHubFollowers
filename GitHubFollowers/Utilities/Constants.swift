92dc7ad7937a8cbf36d4adf7b059d43956c3781a
Joe McCraw <joe@showblender.com>
2020-01-27 17:22:14 -0800

    User Info Header View Controller setup with a babilllion constraints.
    Added Constants for SFSymbols

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Joseph McCraw on 1/27/20.
+//  Copyright © 2020 Joseph McCraw. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}

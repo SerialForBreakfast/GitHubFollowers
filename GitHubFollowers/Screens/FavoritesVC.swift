fd6fa5285467922f50ab3b3ed0e3c101fe1d931e
Joe McCraw <joe@showblender.com>
2020-02-26 08:46:52 -0800

    Persistence Manager for Favorites handling

--- GitHubFollowers/Screens/FavoritesVC.swift
+++ GitHubFollowers/Screens/FavoritesVC.swift
@@ -13,7 +13,15 @@ class FavoritesVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
-        // Do any additional setup after loading the view.
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
     
 

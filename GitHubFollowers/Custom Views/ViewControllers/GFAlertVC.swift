23f671eca12771bf6e05b62954c8a4f1159ccbe9
Joe McCraw <joe@showblender.com>
2020-03-23 17:48:14 -0700

    ContainerView Refactor

--- GitHubFollowers/Custom Views/ViewControllers/GFAlertVC.swift
+++ GitHubFollowers/Custom Views/ViewControllers/GFAlertVC.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class GFAlertVC: UIViewController {
-    let containerView = UIView()
+    let containerView = GFAlertContainerView()
+    
     let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let messageLabel = GFBodyLabel(textAlignment: .center)
     let actionButton = GFButton(backgroundColor: .systemPink, title: "OK")
@@ -46,11 +47,8 @@ class GFAlertVC: UIViewController {
     
     private func configureContainerView() {
         view.addSubview(containerView)
-        containerView.backgroundColor = .systemBackground
-        containerView.layer.cornerRadius = 16
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
         NSLayoutConstraint.activate([
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
